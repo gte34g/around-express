28bf9a966a090775dcc4b6cf844c043a7bc11d26
Ilya Libin <ilyali911@gmail.com>
2022-08-16 21:03:39 +0300

    Adding 404 for non-existent address

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
+
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+const noRoute = require('./routes/noRoute');
 
 const app = express();
 app.get('/', (req, res) => {
@@ -12,9 +14,10 @@ app.get('/', (req, res) => {
         </body>
         </html>`);
 });
+
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
-
+app.use('*', noRoute);
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening at port ${PORT}`);
