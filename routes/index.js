0a4bbbf2694f519d7717c665aa917f172a17f18c
Ilya Libin <ilyali911@gmail.com>
2022-11-21 21:06:08 +0200

    fixing issue

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const cardsRouter = require('./cards');
 const auth = require('../middlewares/auth');
 const NoRoute = require('./noRoute');
 const { createUser, login } = require('../controllers/users');
-const { validateUserBody, validateAuthentication } = require('../middlewares/validation');
+const { validateAuthentication } = require('../middlewares/validation');
 
-router.post('/signup', validateUserBody, createUser);
+router.post('/signup', createUser);
 router.post('/signin', validateAuthentication, login);
 
 router.use(auth);
