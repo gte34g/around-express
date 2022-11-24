9ea0aefe6e587a0eb93ba5c94e2aa7b5b7071223
Ilya Libin <ilyali911@gmail.com>
2022-11-24 22:57:30 +0200

    updating code 2

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -75,11 +75,8 @@ const validateProfile = celebrate({
 
 const validateLogin = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().custom(validateEmail),
+    email: Joi.string().required(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(validateUrl),
   }),
 });
 
