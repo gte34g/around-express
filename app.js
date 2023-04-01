3a67d22da082cb22a034a43c0f03b84377fc704b
Ilya Libin <ilyali911@gmail.com>
2023-04-01 23:46:19 +0300

    validation 2.5.2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 
 require('dotenv').config({ path: './.env' });
@@ -13,15 +14,34 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
-const router = require('./routes/index');
+// const router = require('./routes/index');
+const { createUser, login } = require('./controllers/users');
+const { validateLogin, validateSignup } = require('./middlewares/validation');
+const routes = require('./routes');
 
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/aroundb');
+// mongoose.set('strictQuery', false);
+const url = process.env.CONNECTION_URL.toString();
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connected to MongoDB :)');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB: ', err.message);
+  });
 
 app.use(cors());
 app.options('*', cors());
 
 app.use(requestLogger);
+
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateSignup, createUser);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -29,7 +49,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 app.use(express.json());
-app.use(router);
+// app.use(router);
 app.use(helmet());
 app.use(bodyParser.json());
 
@@ -43,6 +63,9 @@ app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
+
+app.use(routes);
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening at port ${PORT}`);
