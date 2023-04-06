d6933d0afd2d6facdc736e643045e76c9151aa57
Ilya Libin <ilyali911@gmail.com>
2023-04-06 23:48:30 +0300

    fixing registration 7

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-const router = require('express').Router();
-const userRouter = require('./users');
-const cardsRouter = require('./cards');
+// const router = require('express').Router();
+// const userRouter = require('./users');
+// const cardsRouter = require('./cards');
 // const auth = require('../middlewares/auth');
-const NoRoute = require('./noRoute');
+// const NoRoute = require('./noRoute');
 // const { createUser, login } = require('../controllers/users');
 // const validateLogin = require('../middlewares/validation');
 
 // router.post('/signin', validateLogin, login);
 // router.post('/signup', validateLogin, createUser);
 // router.use(auth);
-router.use('/users', userRouter);
-router.use('/cards', cardsRouter);
+// router.use('/users', userRouter);
+// router.use('/cards', cardsRouter);
 
-router.use('*', NoRoute);
+// router.use('*', NoRoute);
 
-module.exports = router;
+// module.exports = router;
