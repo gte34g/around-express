e00be769aa8ec71f63d84a1a7047930c091cdc43
Ilya Libin <ilyali911@gmail.com>
2023-04-08 12:30:42 +0300

    fixing registration 9.7

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../lib/config');
 const Unauthorized = require('../errors/Unauthorized');
+
+// const { JWT_SECRET } = require('../lib/config');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new Unauthorized('You are not authorized'));
   }
-  let payload;
+
   const token = authorization.replace('Bearer ', '');
+  let payload;
   try {
     payload = jwt.verify(
       token,
-      JWT_SECRET,
+      NODE_ENV === 'production' ? JWT_SECRET : 'JWT_SECRET',
     );
   } catch (err) {
     return next(new Unauthorized('You are not authorized'));
