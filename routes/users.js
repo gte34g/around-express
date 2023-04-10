bfa7b2a5631b392a7509e2d7c768a726f568cb3f
Ilya Libin <ilyali911@gmail.com>
2023-04-10 21:45:31 +0300

    removing /users/ from routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,8 @@ const {
 
 router.get('/', authValidation, getUsers);
 router.get('/:id', getUserById);
-router.get('/users/me', authValidation, getCurrentUser);
-router.patch('/users/me', authValidation, updateUser);
-router.patch('/users/me/avatar', authValidation, updateAvatarValidation, updateUserValidation, updateAvatar);
+router.get('/me', authValidation, getCurrentUser);
+router.patch('/me', authValidation, updateUser);
+router.patch('/me/avatar', authValidation, updateAvatarValidation, updateUserValidation, updateAvatar);
 
 module.exports = router;
