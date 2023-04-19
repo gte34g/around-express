350c09eb0ffb80f719ef927d338d87364b5f134e
Ilya Libin <ilyali911@gmail.com>
2023-04-19 23:49:00 +0300

    valiID 3

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -37,9 +37,6 @@ const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().custom(validateEmail),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(validateUrl),
   }),
 });
 
